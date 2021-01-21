67cd90742bd80b684e3cedacfc96a8397b4e8d10
Luis Fernando Trindade <lfernandotc@hotmail.com>
2021-01-21 11:01:22 -0300

    first commit

---
+++ upload/setup/inc/footer.inc.php
@@ -0,0 +1,12 @@
+
+            <div class="clear"></div>
+        </div> <!-- content -->
+    </div> <!-- wizard -->
+    <div id="footer" class="centered">Copyright &copy; <?php echo date('Y'); ?> <a target="_blank" href="https://osticket.com">osTicket.com</a></div>
+
+    <script type="text/javascript" src="../js/jquery-3.5.1.min.js?d5ee0df"></script>
+    <script type="text/javascript" src="../js/jstz.min.js?d5ee0df"></script>
+    <script type="text/javascript" src="js/setup.js?d5ee0df"></script>
+    <script type="text/javascript" src="js/tips.js?d5ee0df"></script>
+</body>
+</html>
